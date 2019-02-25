711badbc408b8d8a1b294f5d43b78549fca2ac87
niildu <niildu@gmail.com>
2019-02-25 23:13:16 +0600

    Updates

---
+++ precache-manifest.1c277b705d38f750c91d8f7bf42c0413.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0945be15e27dd30a16be",
+    "url": "/react-todo-app/static/js/runtime~main.0945be15.js"
+  },
+  {
+    "revision": "b913955d98414d40fc47",
+    "url": "/react-todo-app/static/js/main.b913955d.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/react-todo-app/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b913955d98414d40fc47",
+    "url": "/react-todo-app/static/css/main.9c026d3a.chunk.css"
+  },
+  {
+    "revision": "b44711b235421613a56a0b21fe65a9fa",
+    "url": "/react-todo-app/index.html"
+  }
+];
